030a46672b3b5528aedc9388ef9138995a0978f8
kaznak <kaznak.at.work@gmail.com>
2020-10-04 22:40:14 +0900

    api/graphql/Post.ts: add a mutation schema

--- api/graphql/Post.ts
+++ api/graphql/Post.ts
@@ -1,5 +1,5 @@
 // api/graphql/Post.ts
-import { objectType, extendType } from "@nexus/schema";
+import { objectType, extendType, stringArg } from "@nexus/schema";
 
 export const PostQuery = extendType({
   type: "Query",
@@ -22,5 +22,29 @@ export const Post = objectType({
     t.string("title"); // <- Field named `title` of type `String`
     t.string("body"); // <- Field named `body` of type `String`
     t.boolean("published"); // <- Field named `published` of type `Boolean`
+  },
+});
+
+export const PostMutation = extendType({
+  type: "Mutation",
+  definition(t) {
+    t.field("createDraft", {
+      type: "Post",
+      nullable: false,
+      args: {
+        title: stringArg({ required: true }),
+        body: stringArg({ required: true }),
+      },
+      resolve(_root, args, ctx) {
+        const draft = {
+          id: ctx.db.posts.length + 1,
+          title: args.title,
+          body: args.body,
+          published: false,
+        };
+        ctx.db.posts.push(draft);
+        return draft;
+      },
+    });
   },
 });
