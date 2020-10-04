8f7fcb385ce7fac45bf767c861af9763a503b607
kaznak <kaznak.at.work@gmail.com>
2020-10-04 20:38:59 +0900

    api/schema.ts: update schema

--- api/schema.ts
+++ api/schema.ts
@@ -1,9 +1,10 @@
 // api/schema.ts
 import { makeSchema } from "@nexus/schema";
 import { join } from "path";
+import * as typeDefs from "./graphql";
 
 export const schema = makeSchema({
-  types: [], // 1
+  types: typeDefs,
   outputs: {
     typegen: join(__dirname, "..", "nexus-typegen.ts"), // 2
     schema: join(__dirname, "..", "schema.graphql"), // 3
