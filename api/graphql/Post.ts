93199895e7411b8103aa4d87531bcaf6bd6115b8
kaznak <kaznak.at.work@gmail.com>
2020-10-04 20:37:49 +0900

    api/graphql/Post.ts: add

---
+++ api/graphql/Post.ts
@@ -0,0 +1,12 @@
+// api/graphql/Post.ts
+import { objectType } from "@nexus/schema";
+
+export const Post = objectType({
+  name: "Post", // <- Name of your type
+  definition(t) {
+    t.int("id"); // <- Field named `id` of type `Int`
+    t.string("title"); // <- Field named `title` of type `String`
+    t.string("body"); // <- Field named `body` of type `String`
+    t.boolean("published"); // <- Field named `published` of type `Boolean`
+  },
+});
